830bb42bb3170b773653788a8435d8dc0a7dfa71
Simon Freedland <simon.freedland@nology.io>
2019-10-18 09:46:07 +0100

    first version getting a map to show

---
+++ src/js/main.ts
@@ -0,0 +1,10 @@
+const initMap = () => {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), { zoom: 8, center: uluru });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({ position: uluru, map: map });
+};
+
+initMap();
